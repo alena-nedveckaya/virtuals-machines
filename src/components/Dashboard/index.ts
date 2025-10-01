632d7437c836ea4d0d997ad0ec8fa4a978a7cc31
Alena <alenashcherb@gmail.com>
2025-10-01 23:19:43 +0200

    fix: refactoring

--- src/components/Dashboard/index.ts
+++ src/components/Dashboard/index.ts
@@ -4,3 +4,10 @@ export { default as VMChart } from './VMChart';
 export { default as VMStats } from './VMStats';
 export { default as VMTable } from './VMTable';
 export { default as VMWizard } from './VMWizard';
+
+export { default as VMTableHeader } from './VMTable/VMTableHeader';
+export { default as VMTableRow } from './VMTable/VMTableRow';
+
+export * from './types';
+export * from './hooks';
+export * from './utils';
